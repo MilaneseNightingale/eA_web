46478f4b872c1297973d36a2fce9ee745522fe3b
Rajneesh0045 <rajneeshkumarsingh0045@gmail.com>
2018-04-28 16:37:48 +0530

    To test api call

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
-var eventDetails = [
+var mainEventType  = [
 	{
 		name: 'Social',
 		pic: "img/social.jpg",
-		sub: ["birthday","marriage","party"]
+		sub: ["birthday","marriage","party","birthday","marriage","party"]
 	},
 	{
 		name: 'Tech',
 		pic: "img/tech.jpg",
-		sub: ["talk","Hackathon","Session","Workshop"]
+		sub: ["talk","Hackathon","Session","Workshop","talk","Hackathon","Session","Workshop"]
 	},
 	{
 		name: 'Trip',
 		pic: "img/trip.jpg",
-		sub: ["Picnic","Trek","Road Trip","site seeing"]
+		sub: ["Picnic","Trek","Road Trip","site seeing","Picnic","Trek","Road Trip","site seeing"]
 	},
 	{
 		name: 'Cultural',
@@ -41,33 +41,108 @@ var eventDetails = [
 	}
 ];
 
+
+var subEvents = [
+	{
+		name: "birthday",
+		sub: ["birthday 1","birthday 2","birthday 3","birthday 4","birthday 5","birthday 6","birthday 7","birthday 8","birthday 9","birthday 10"]
+	},
+	{
+		name: "marriage",
+		sub: ["marriage 1","marriage 2","marriage 3","marriage 4","marriage 5","marriage 6","marriage 7","marriage 8","marriage 9","marriage 10"]
+	}
+];
+
+
+
+
+
 // populating the page with main events
+const main = document.querySelector(".main");
 
-const eA_events = document.querySelector("#events");
 
-function eA_mainEventPopulate(eventDetails) {
+function eA_mainEventPopulate(mainEventType ) {
 	
-		for (let i = 0; i < eventDetails.length; i++) {
-			
-			eA_events.innerHTML += '<div class="eventMain" data-num="'+ i +'"\
-			style="background-image: url('+ eventDetails[i].pic +');">\
-			<h4 data-num="' + i + '">' + eventDetails[i].name + '</h4>\
-			</div>';
+	if(window.innerWidth < 900){
+		main.innerHTML ='<div class="directory">\
+				<i class="fas fa-arrow-left"></i>\
+				<span data-href="mainEventType " >home /</span>\
+			</div>'
+		main.innerHTML += '<div class="events"></div>';
+		for (let i = 0; i < mainEventType .length; i++) {
+			document.querySelector('.events').innerHTML += '<div class="eventMain" style="background-image: url(' + mainEventType [i].pic + ');" data-num="'+ i +'"><h4 data-num="' + i + '">' + mainEventType [i].name + '</h4></div>';
 		}
+	}
+	if(window.innerWidth >= 900){
+		var mainEventBar = document.querySelector('.mainEvent-sidebar');
+		for (let i = 0; i < mainEventType .length; i++) {
+			mainEventBar.innerHTML += '<div class="mainEvent" data-num='+ i +'><p data-num='+ i +'>' + mainEventType [i].name + '</p>\</div>'
+		}
+	}
 }
-eA_mainEventPopulate(eventDetails);
+eA_mainEventPopulate(mainEventType );
 
+// main event to sub event
 
 $(".eventMain").click(function(e) {
-	eA_events.innerHTML = '';
-	let subIndex = e.target.getAttribute("data-num");
-	for(let i = 0; i < eventDetails[subIndex].sub.length; i++ ){
-		eA_events.innerHTML += '<div class="sub"><p>' + eventDetails[subIndex].sub[i] + '</p></div>';
+	if(window.innerWidth <= 899){
+		document.querySelector('.events') .innerHTML= '';
 	}
-	document.querySelector(".directory").innerHTML += '<span data-href="'+ eventDetails[subIndex].name +'" > ' + eventDetails[subIndex].name +' /</span>';
+	let subIndex = e.target.getAttribute("data-num");
+	document.querySelector('.events').innerHTML += '<div class="sub-group"></div>';
+	for(let i = 0; i < mainEventType [subIndex].sub.length; i++ ){
+		document.querySelector('.sub-group').innerHTML += '<div class="sub"><p>' + mainEventType [subIndex].sub[i] + '</p></div>';
+	}
+	document.querySelector(".directory").innerHTML += '<span data-href="'+ mainEventType [subIndex].name +'" > ' + mainEventType [subIndex].name +' /</span>';
 	dirClick();
 	dirArrayUpdate();
+	subEventUpdates();
 });
+
+$(".mainEvent").click(function(e) {
+	main.innerHTML ='<div class="directory"><i class="fas fa-arrow-left"></i></div>';
+	let subIndex = e.target.getAttribute("data-num");
+	main.innerHTML += '<div class="sub-group grid-rand'+ Math.floor(Math.random() * Math.floor(3)) +'"></div>';
+	for(let i = 0; i < mainEventType [subIndex].sub.length; i++ ){
+		document.querySelector('.sub-group').innerHTML += '<div class="sub"><p>' + mainEventType [subIndex].sub[i] + '</p></div>';
+	}
+	document.querySelector(".directory").innerHTML += '<span data-href="'+ mainEventType [subIndex].name +'" > ' + mainEventType [subIndex].name +' /</span>';
+	dirClick();
+	dirArrayUpdate();
+	subEventUpdates();
+});
+
+
+// Sub event clicks 
+function subEventUpdates(){
+	$(".sub").click(function(e) {
+	
+		let currentSub = e.delegateTarget.getElementsByTagName("p")[0].innerText;
+		var foundSub = 0;
+		document.querySelector('.sub-group') .innerHTML= '';
+		$.each(subEvents, function(key, value){
+			if(value.name.search(currentSub) != -1){
+				foundSub = 1
+				for(let i = 0; i < value.sub.length; i++ ){
+					document.querySelector('.sub-group').innerHTML += '<div class="sub"><p>' + value.sub[i] + '</p></div>';
+				}
+				document.querySelector(".directory").innerHTML += '<span data-href="'+ value.name +'" > ' + value.name +' /</span>';
+			}
+		});
+		
+		if(foundSub != 1){
+			document.querySelector('.sub-group').innerHTML += '<p>You have reached the end</p>';
+		}
+		dirClick();
+		dirArrayUpdate();
+		subEventUpdates();
+	});	
+}
+
+
+
+
+
 
 //  Directory structure in events
 
@@ -76,7 +151,25 @@ function dirClick() {
 		console.log(e);
 		console.log(e.currentTarget.attributes[0].value);		
 	});
-}; dirClick();
+};
+dirClick();
+
+// For Directory structure
+
+var eA_dirArray = new Array();
+// $(window).on('load', dirArrayUpdate());
+
+function dirArrayUpdate(){
+	var dir = document.querySelector('.directory').innerText;
+	eA_dirArray = dir.split('/');
+	for(var i = eA_dirArray.length - 1; i >= 0; i--) {
+		if(eA_dirArray[i] === "") {
+			eA_dirArray.splice(i, 1);
+		}
+		// eA_dirArray[i] = eA_dirArray[i].trim();
+	}
+	console.log(eA_dirArray);
+}
 
 // Fab button for navActivity on mobile
 
@@ -101,6 +194,7 @@ $('.backLight').click(function() {
 	$('#navActivity').slideUp();
 	$('.backLight').fadeOut();
 });
+
 
 $('.newEvents').click(function() {
 	document.getElementById("navPopup").innerHTML = '\
@@ -167,7 +261,7 @@ $('.myEvents').click(function(){
 			date: '30-Mar'
 		},
 		{
-			name: 'Prashant\'s mirrage',
+			name: 'Prashant\'s marrige',
 			date: '07-Apr'
 		},
 		{
@@ -186,30 +280,73 @@ $('.myEvents').click(function(){
 	$('.navPopup').slideDown();
 	$('.backDark').fadeIn();
 });
+
 $('.backDark').click(function() {
+	$('.navPopup').slideUp();
+	$('.backDark').fadeOut();
+});
+$("#navClose").click(function() {
 	$('.navPopup').slideUp();
 	$('.backDark').fadeOut();
 });
 
 
 
-var eA_dirArray = new Array();
-$(window).on('load', dirArrayUpdate());
-function dirArrayUpdate(){
-	var dir = document.querySelector('.directory').innerText;
-	eA_dirArray = dir.split('/');
-	console.log(eA_dirArray);
+// Populate the results of shop and eventhandlers
+
+		// code here
+
+// Shop details pages
+
+$('.result-card').click(function(e){
+	if(e.currentTarget.classList[0] === 'shop'){
+		// Call shop details function
+	}
+	if(e.currentTarget.classList[0] === 'eventhandler'){
+		// call eventhandler details
+	}
+});
+
+function shopDetails(){
+	// main.innerHTML
+}
+function eventhandlerDetails(){
+	// code here
 }
 
 
 
-window.addEventListener('load', async e =>{
-	if('serviceWorker' in navigator){
-		try{
-			navigator.serviceWorker.register('sw.js');
-			console.log('SW registered');
-		} catch(error){
-			console.log('SW registration failed');
-		}
-	}
+// my event detai
+$('.myEvent-card').click(function(e){
+
+	
+	document.querySelector("#main").innerHTML = e.currentTarget.innerHTML;
 });
+
+
+
+
+// Post calls 
+
+var requestVar = '{ "message" : "somethimggggg", "This" : "ISsssssss" }';
+
+$.post("https://webhook.site/b96c4f6c-fbfc-4a1f-b9ec-98dc8a251c89",requestVar, function(data){
+	console.log(data);
+},"JSON");
+
+
+
+
+
+// Service worker
+
+// window.addEventListener('load', async e =>{
+// 	if('serviceWorker' in navigator){
+// 		try{
+// 			navigator.serviceWorker.register('sw.js');
+// 			console.log('SW registered');
+// 		} catch(error){
+// 			console.log('SW registration failed');
+// 		}
+// 	}
+// });
